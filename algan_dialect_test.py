57432f3ce8b64676295bdf90fc0629a22b526be7
mbakcan211 <mustafa.akcan.b8@gmail.com>
2026-01-28 22:39:27 +0300

    Send custom RFD_TEST message with fixed random number data

--- algan_dialect_test.py
+++ algan_dialect_test.py
@@ -4,7 +4,7 @@ from pymavlink import mavutil
 from generated_build.python import algan_uav
 
 parser = argparse.ArgumentParser(description='Send custom MAVLink messages.')
-parser.add_argument('--device', default='udpin:127.0.0.1:14540', help='Device path (e.g., /dev/ttyUSB0) or connection string')
+parser.add_argument('--device', default='udpin:127.0.0.1:14550', help='Device path (e.g., /dev/ttyUSB0) or connection string')
 parser.add_argument('--baud', type=int, default=57600, help='Baud rate for serial connection')
 args = parser.parse_args()
 
@@ -20,6 +20,11 @@ print("Heartbeat received! Sending RFD_TEST messages...")
 
 # Send custom RFD_TEST message with fixed random number data
 # Field: RANDOM_DEGER (uint8_t)
-mav.rfd_test_send(RANDOM_DEGER=42)
-print("Message SENT!")
+
+try:
+    mav.rfd_test_send(RANDOM_DEGER=int(input("RFD_TEST RANDOM_DEGER = ")))
+    print("Message SENT!")
+except Exception as e:
+    print(f"mav.rf_test_send HATA --->> {e}")
+
 time.sleep(1)
